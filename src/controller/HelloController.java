0528727bb73ed29c5e16b0e1864e6c40c367050b
wuzf@cnmobi.cn <w615324d>
2020-12-09 09:58:53 +0800

    第五个版本

--- src/controller/HelloController.java
+++ src/controller/HelloController.java
@@ -14,4 +14,12 @@ public class HelloController {
     public void test2() {
         System.out.println("这里是测试版本回退！！！=========2");
     }
+
+    public void test3() {
+        System.out.println("这里是测试版本回退！！！=========3");
+    }
+
+    public void test4() {
+        System.out.println("这里是测试版本回退！！！=========4");
+    }
 }
