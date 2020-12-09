2d066b056d5943f836e0b6c67f250d462ee8cc2c
wuzf@cnmobi.cn <w615324d>
2020-12-09 09:50:44 +0800

    Revert "第一个版本"
    This reverts commit 9021904a

--- src/controller/HelloController.java
+++ src/controller/HelloController.java
@@ -6,12 +6,4 @@ public class HelloController {
     public void test() {
         System.out.println("这里是测试版本回退！！！");
     }
-
-    public void test1() {
-        System.out.println("这里是测试版本回退！！！=========1");
-    }
-
-    public void test2() {
-        System.out.println("这里是测试版本回退！！！=========2");
-    }
 }
